c4e9637f0aba2c0bbe0c56b7daa6b7a86d0cb8ff
Sive func <sivefunc@tuta.io>
2022-09-01 19:14:08 +0000

    Replace GameParser.py

--- 2048/GameParser.py
+++ 2048/GameParser.py
@@ -13,7 +13,7 @@ def game_parser():
             action='version',
             version="""
 %(prog)s v1.0.0
-Copyright (C) 2022 Vryepa
+Copyright (C) 2022 Sivefunc
 License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>
 This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.
